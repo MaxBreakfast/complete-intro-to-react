6394d8010233b19858d537b72a2a647fe9f82abe
Max Ince <ince.max@gmail.com>
2016-10-07 13:22:18 -0700

    fixed webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,39 +1,26 @@
-  /* global React ReactDOM */
-  var div = React.DOM.div
-  var h1 = React.DOM.h1
+/* global React ReactDOM */
+var div = React.DOM.div
+var MyTitle = require('./MyTitle')
 
-  var MyTitle = React.createClass({
-    render () {
-      return (
-        div(null,
-          h1({
-            style: {
-              color: this.props.color
-            }
-          }, this.props.title)
-        )
-      )
-    }
-  })
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
-  var MyTitleFact = React.createFactory(MyTitle)
+var MyFirstComponent = (
+  div(null,
+    MyTitleFact({
+      title: 'props are great',
+      color: 'rebeccapurple'
+    }),
+    MyTitleFact({
+      title: 'use props everywhere',
+      color: 'mediumaquamarine'
+    }),
+    ce(MyTitle, {
+      title: 'props',
+      color: 'papayawhip'
+    })
 
-  var MyFirstComponent = (
-    div(null,
-      MyTitleFact({
-        title: 'props are great',
-        color: 'rebeccapurple'
-      }),
-      MyTitleFact({
-        title: 'use props everywhere',
-        color: 'mediumaquamarine'
-      }),
-      MyTitleFact({
-        title: 'props',
-        color: 'papayawhip'
-      })
-
-    )
   )
+)
 
-  ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
