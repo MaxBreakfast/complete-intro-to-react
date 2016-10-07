8a4c6acde2a53c47a09a4c589de88bb24f1dd90b
Max Ince <ince.max@gmail.com>
2016-10-07 12:24:39 -0700

    added editorconfig and standard

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,40 +1,39 @@
-var div = React.DOM.div
-var h1 = React.DOM.h1
-var h2 = React.DOM.h2
+  /* global React ReactDOM */
+  var div = React.DOM.div
+  var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-  render() {
-    return (
-      div(null,
-        h1({
-          style: {
-            color: this.props.color
-          }
-        }, this.props.title),
-        h2(null, 'test')
+  var MyTitle = React.createClass({
+    render () {
+      return (
+        div(null,
+          h1({
+            style: {
+              color: this.props.color
+            }
+          }, this.props.title)
+        )
       )
+    }
+  })
+
+  var MyTitleFact = React.createFactory(MyTitle)
+
+  var MyFirstComponent = (
+    div(null,
+      MyTitleFact({
+        title: 'props are great',
+        color: 'rebeccapurple'
+      }),
+      MyTitleFact({
+        title: 'use props everywhere',
+        color: 'mediumaquamarine'
+      }),
+      MyTitleFact({
+        title: 'props',
+        color: 'papayawhip'
+      })
+
     )
-  }
-})
-
-var MyTitleFact = React.createFactory(MyTitle)
-
-var MyFirstComponent = (
-  div(null,
-    MyTitleFact({
-      title: 'props are great',
-      color: 'rebeccapurple',
-    }),
-    MyTitleFact({
-      title: 'use props everywhere',
-      color: 'mediumaquamarine',
-    }),
-    MyTitleFact({
-      title: 'props',
-      color: 'papayawhip',
-    })
-
   )
-)
 
-ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+  ReactDOM.render(MyFirstComponent, document.getElementById('app'))
