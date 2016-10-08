d9d8cf02b272773059d1f3acb787ae75a89f6d11
Max Ince <ince.max@gmail.com>
2016-10-07 21:15:04 -0700

    converted ClientApp.js to jsx format

---
+++ js/ClientApp.jsx
@@ -0,0 +1,16 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
+
+
+var MyFirstComponent =  function () {
+  return (
+    <div>
+      <MyTitle title='whatevs' color='rebeccapurple' />
+      <MyTitle title='derp' color='mediumaquamarine' />
+      <MyTitle title='extra derpderp' color='papayawhip' />
+    </div>
+  )
+}
+
+ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
