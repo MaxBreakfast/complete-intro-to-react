d39e2d26f41d7a87a42d65110ba1eb6823f25ada
Max Ince <ince.max@gmail.com>
2016-10-07 22:52:57 -0700

    setup eslintrc and linting during webpack process

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,11 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
-
-
 const App = () => {
   return (
     <div className='app-container'>
       <div className='home-info'>
-        <h1 className='title'>svideo</h1>
+        <h1 className='title'>svideo!?!?!</h1>
         <input className='search' type='text' placeholder='search' />
         <button className='browse-all'> or Browse All</button>
       </div>
