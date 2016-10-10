399564a5b33b7e8058bef67672d8ed279febbf38
Max Ince <ince.max@gmail.com>
2016-10-10 15:34:50 -0700

    added some light state management

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,12 +3,21 @@ const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'default search term'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
   render () {
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>svideo</h1>
-          <input className='search-input' type='text' placeholder='Search' />
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search'
+            onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
           {data.shows.map(show => (
