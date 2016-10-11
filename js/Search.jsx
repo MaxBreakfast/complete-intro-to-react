4edbba2ca6069d1816d26a6ad430cf6e391389b4
Max Ince <ince.max@gmail.com>
2016-10-10 17:51:25 -0700

    implemented search filter

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,7 +5,7 @@ const ShowCard = require('./ShowCard')
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'default search term'
+      searchTerm: ''
     }
   },
   handleSearchTermEvent (event) {
@@ -15,13 +15,17 @@ const Search = React.createClass({
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>{this.state.searchTerm}</h1>
-          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search'
-            onChange={this.handleSearchTermEvent} />
+          <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text'
+            placeholder='Search' onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
-          {data.shows.map(show => (
-            <ShowCard {...show} key={show.imdbID} />
+          {data.shows
+            .filter(show =>
+              `${show.title} ${show.description}`.toUpperCase()
+              .indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map(show => (
+              <ShowCard {...show} key={show.imdbID} />
           ))}
         </div>
       </div>
